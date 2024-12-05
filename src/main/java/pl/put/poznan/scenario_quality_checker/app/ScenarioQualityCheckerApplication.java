22d3e0e3b42534f4c3fec17973b1fe5f2f1f2e8c
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2024-12-05 21:32:34 +0100

    rename default project name to ScenarioQualityChecker

---
+++ src/main/java/pl/put/poznan/scenario_quality_checker/app/ScenarioQualityCheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.put.poznan.scenario_quality_checker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.scenario_quality_checker.rest"})
+public class ScenarioQualityCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScenarioQualityCheckerApplication.class, args);
+    }
+}
